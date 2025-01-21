a8d73fac0abbf27a7fd81ebe9b5310f903ef87dd
Utkarsh Vishwas <utkarshvishwas@gmail.com>
2025-01-21 23:29:39 +0530

    conitnuing the firstsartup page
    fixeed the styling of all the tabs nad added the selected folders as main tab and rowse directory to add folders as a modal option. work for posting the startup data is pending

--- src/pages/FirstStartUp.js
+++ src/pages/FirstStartUp.js
@@ -1,6 +1,15 @@
 import { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { FileBlank, Folder, FolderMinusSolid, Hdd } from "../assets";
+import {
+  ArrowBack,
+  FileBlank,
+  Folder,
+  Hdd,
+  ToLogo,
+  WelcomeLogo,
+  Plus,
+  FolderMinusSolid,
+} from "../assets";
 
 const Welcome = () => (
   <motion.div
@@ -10,8 +19,12 @@ const Welcome = () => (
     transition={{ duration: 0.3 }}
     className="intro-container"
   >
-    <div className="welcome">Welcome</div>
-    <div className="to">to</div>
+    <div className="welcome">
+      <WelcomeLogo />
+    </div>
+    <div className="to">
+      <ToLogo />
+    </div>
     <div className="vidya">VIDYA</div>
     <div className="server-tagline">A Media Server for video lectures</div>
     <div className="server-description">
@@ -38,32 +51,59 @@ const Username = ({
     <div className="admin-setup">
       <div className="username-title">Create a Admin Username & Password</div>
     </div>
-    <div className="username-label">Username</div>
-    <div className="username-input">
-      <input
-        onChange={(e) => setUsernameInput(e.target.value)}
-        value={usernameInput}
-        type="text"
-      />
-    </div>
-    <div className="password-label">Password</div>
-    <div className="password-input">
-      <input
-        onChange={(e) => setPasswordInput(e.target.value)}
-        value={passwordInput}
-        type="password"
-        name=""
-        id=""
-      />
+    <div className="input-container">
+      <div className="username-label">Username</div>
+      <div className="username-input">
+        <input
+          onChange={(e) => setUsernameInput(e.target.value)}
+          value={usernameInput}
+          type="text"
+        />
+      </div>
+      <div className="password-label">Password</div>
+      <div className="password-input">
+        <input
+          onChange={(e) => setPasswordInput(e.target.value)}
+          value={passwordInput}
+          type="password"
+          name=""
+          id=""
+        />
+      </div>
     </div>
   </motion.div>
 );
-
-const FileBrowser = () => {
+const SelectedFolders = ({ selectedFolders, onAddFolder, onRemoveFolder }) => (
+  <motion.div
+    initial={{ opacity: 0, x: -20 }}
+    animate={{ opacity: 1, x: 0 }}
+    exit={{ opacity: 0, y: -20 }}
+    transition={{ duration: 0.3 }}
+    className="selected-folders-container"
+  >
+    <div className="folders-setup">
+      <div className="folders-title">Add Folders to scan for lectures</div>
+    </div>
+    <div className="folders-list">
+      {selectedFolders.map((folder, index) => (
+        <div key={index} className="selected-folder">
+          <div className="folder-path">{folder}</div>
+          <div onClick={() => onRemoveFolder(index)} className="remove-folder">
+            <FolderMinusSolid />
+          </div>
+        </div>
+      ))}
+    </div>
+    <div onClick={onAddFolder} className="add-folder-button">
+      <Plus />
+      Add Folder
+    </div>
+  </motion.div>
+);
+const FileBrowser = ({ onSelect }) => {
   const [currentPath, setCurrentPath] = useState("");
   const [items, setItems] = useState([]);
   const [drives, setDrives] = useState([]);
-  const [selectedPath, setSelectedPath] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [parentDirectory, setParentDirectory] = useState(null);
@@ -75,7 +115,6 @@ const FileBrowser = () => {
       if (!response.ok) throw new Error("Failed to fetch drives");
       const data = await response.json();
       setDrives(data);
-      // If no current path is set, use the first accessible drive
       if (!currentPath && data.length > 0) {
         const firstAccessibleDrive = data.find((drive) => drive.accessible);
         if (firstAccessibleDrive) {
@@ -114,23 +153,6 @@ const FileBrowser = () => {
     }
   };
 
-  const savePath = async () => {
-    try {
-      const response = await fetch("http://192.168.1.25:3001/api/save-path", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ selectedPath }),
-      });
-      if (!response.ok) throw new Error("Failed to save path");
-      const data = await response.json();
-      alert(`Path saved successfully: ${data.savedPath}`);
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
   const formatFileSize = (bytes) => {
     if (bytes === null) return "N/A";
     const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
@@ -150,7 +172,7 @@ const FileBrowser = () => {
       <div className="drives">
         {drives.map((drive) => (
           <div
-            key={drive.path}
+            key={drive.label}
             onClick={() => drive.accessible && fetchDirectory(drive.path)}
             className={`drive ${
               drive.accessible ? "accessible-drive" : "not-allowed-drive"
@@ -168,32 +190,43 @@ const FileBrowser = () => {
           </div>
         ))}
       </div>
-      <div className="current-path">{currentPath}</div>
+      <div className="current-path">Current Path - {currentPath}</div>
       {parentDirectory && (
         <div className="parent-directory" onClick={handleBack}>
           ...
         </div>
       )}
 
-      {loading && <div className="loading">Loading...</div>}
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="folder-list">
+          {items.map((item) => (
+            <div
+              key={item.path}
+              className="items"
+              onClick={() =>
+                item.isDirectory ? fetchDirectory(item.path) : ""
+              }
+            >
+              {item.isDirectory ? <Folder /> : <FileBlank />}
+              <div className="item-lable">{item.name}</div>
+              {!item.isDirectory && (
+                <span className="item-size">{formatFileSize(item.size)}</span>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
       {error && <div className="error">{error}</div>}
-      <div className="folder-list">
-        {items.map((item) => (
-          <div
-            className="items"
-            onClick={() =>
-              item.isDirectory
-                ? fetchDirectory(item.path)
-                : setSelectedPath(item.path)
-            }
-          >
-            {item.isDirectory ? <Folder /> : <FileBlank />}
-            <div className="item-lable">{item.name}</div>
-            {!item.isDirectory && (
-              <span className="item-size">{formatFileSize(item.size)}</span>
-            )}
-          </div>
-        ))}
+      <div className="select-folder">
+        <div
+          onClick={() => {
+            onSelect(currentPath);
+          }}
+        >
+          Select
+        </div>
       </div>
     </div>
   );
@@ -201,20 +234,15 @@ const FileBrowser = () => {
 
 const FirstStartUp = () => {
   const [currentTab, setCurrentTab] = useState(0);
-
   const [usernameInput, setUsernameInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
+  const [selectedFolders, setSelectedFolders] = useState([]);
+  const [isChoosingFolder, setIsChoosingFolder] = useState(false);
 
   const tabs = [
-    {
-      title: "Welcome",
-    },
-    {
-      title: "Username",
-    },
-    {
-      title: "Drive",
-    },
+    { title: "Welcome" },
+    { title: "Username" },
+    { title: "Selected Folders" },
   ];
 
   const handleNext = () => {
@@ -222,7 +250,36 @@ const FirstStartUp = () => {
   };
 
   const handleBack = () => {
-    if (currentTab > 0) setCurrentTab(currentTab - 1);
+    if (isChoosingFolder) {
+      setIsChoosingFolder(false);
+    } else if (currentTab > 0) {
+      setCurrentTab(currentTab - 1);
+    }
+  };
+
+  const handleAddFolder = () => {
+    setIsChoosingFolder(true);
+  };
+
+  const handleFolderSelected = (path) => {
+    if (selectedFolders.includes(path)) {
+      setIsChoosingFolder(false);
+      return;
+    }
+    setSelectedFolders([...selectedFolders, path]);
+    setIsChoosingFolder(false);
+  };
+
+  const handleRemoveFolder = (index) => {
+    setSelectedFolders(selectedFolders.filter((_, i) => i !== index));
+  };
+
+  const ModifiedFileBrowser = () => (
+    <FileBrowser onSelect={handleFolderSelected} />
+  );
+
+  const handleFinish = () => {
+    console.log(selectedFolders, usernameInput, passwordInput);
   };
 
   return (
@@ -230,20 +287,45 @@ const FirstStartUp = () => {
       <div className="startup-wrap">
         <div className="first-startup-container">
           <AnimatePresence mode="wait">
-            {currentTab === 0 && <Welcome />}
-            {currentTab === 1 && (
-              <Username
-                usernameInput={usernameInput}
-                setUsernameInput={setUsernameInput}
-                passwordInput={passwordInput}
-                setPasswordInput={setPasswordInput}
-              />
+            {!isChoosingFolder ? (
+              <>
+                {currentTab === 0 && <Welcome />}
+                {currentTab === 1 && (
+                  <Username
+                    usernameInput={usernameInput}
+                    setUsernameInput={setUsernameInput}
+                    passwordInput={passwordInput}
+                    setPasswordInput={setPasswordInput}
+                  />
+                )}
+                {currentTab === 2 && (
+                  <SelectedFolders
+                    selectedFolders={selectedFolders}
+                    onAddFolder={handleAddFolder}
+                    onRemoveFolder={handleRemoveFolder}
+                  />
+                )}
+              </>
+            ) : (
+              <ModifiedFileBrowser />
             )}
-            {currentTab === 2 && <FileBrowser />}
           </AnimatePresence>
           <div className="setup-buttons">
-            {currentTab > 0 && <button onClick={handleBack}>Back</button>}
-            <button onClick={handleNext}>Next</button>
+            {(currentTab > 0 || isChoosingFolder) && (
+              <div className="back-button" onClick={handleBack}>
+                <ArrowBack /> Back
+              </div>
+            )}
+            {!isChoosingFolder &&
+              (currentTab === 2 ? (
+                <div className="next-button" onClick={handleFinish}>
+                  Finish <ArrowBack />
+                </div>
+              ) : (
+                <div className="next-button" onClick={handleNext}>
+                  Next <ArrowBack />
+                </div>
+              ))}
           </div>
         </div>
       </div>
